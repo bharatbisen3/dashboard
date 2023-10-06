2b7f6b524dee84f9819128f43d96a36bd56a1f78
bharatbisen3 <bharatbisen3@gmail.com>
2023-10-06 13:59:37 +0530

    first commit

---
+++ dashboard-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Demo from './Components/Demo';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1> Hello MUI </h1>
+
+      <Demo />
+
+    </div>
+  );
+}
+
+export default App;
